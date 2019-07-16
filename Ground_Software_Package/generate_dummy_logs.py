5c888b04a476df794b708c8418158b40b0373d51
krd020 <krd020@mail.usask.ca>
2019-07-16 15:03:47 -0600

    These currently work with the data from last year that was provided in google drive.

--- Ground_Software_Package/generate_dummy_logs.py
+++ Ground_Software_Package/generate_dummy_logs.py
@@ -1,14 +1,14 @@
 # generate log files to test live plots
 import time
 
-# get the data to use
-file_name = r'C:\Users\puetz\Desktop\livetest.txt'
+# get the data to use: data file from last year's flight
+file_name = r'C:/Users/kimdu/Documents/ph549/20180709_data.txt'
 data = open(file_name, 'rb').readlines()
 for i in range(len(data)):
     data[i] = data[i].decode()[:-2]
 
 # save the data at a normal rate
-fil = r'C:\Users\puetz\Desktop\Telemtry_logs\Test\test.txt'
+fil = r'C:/Users/kimdu/Documents/ph549/Telemetry_logs/test.txt'
 for i in range(len(data)):
     with open(fil, 'a') as file:
         file.write(data[i]+"\n")
